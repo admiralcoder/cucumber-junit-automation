08da78d0cd6bd5a7141829500a8d0c163df6daef
admiralcoder <djale2008@icloud.com>
2018-03-17 16:52:05 -0400

    updated couple of files

--- first-cucumber-project/src/test/java/tesla/HomePageStepDefinitions.java
+++ first-cucumber-project/src/test/java/tesla/HomePageStepDefinitions.java
@@ -1,44 +1,57 @@
 package tesla;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HomePageStepDefinitions {
-	// this is a regular expression / regex that cucumber 
+	// this is a regular expression / regex that cucumber
 	// uses to match a method to a cucumber step
-	
 
 	WebDriver driver;
 
-	
 	@Given("^I am on the home page$")
 	public void i_am_on_the_home_page() {
 		WebDriverManager.chromedriver().setup();
-	    System.out.println("I am going to www.tesla.com");
-	    driver = new ChromeDriver();
-	    driver.get("https://www.tesla.com/");
-	    
+		System.out.println("I am going to www.tesla.com");
+		driver = new ChromeDriver();
+		driver.get("https://www.tesla.com/");
+
 	}
 
 	@When("^I click on the model S link$")
 	public void i_click_on_the_model_S_link() {
-	    System.out.println("clicking on the model S link");
-	    driver.findElement(By.linkText("MODEL S")).click();;
+		System.out.println("clicking on the model S link");
+		driver.findElement(By.linkText("MODEL S")).click();
+		;
 
-	    
 	}
 
 	@Then("^Model S homepage should be displayed$")
 	public void model_S_homepage_should_be_displayed() {
-	    System.out.println("Verifying the Model S home Page");
-	    
-	   
+		System.out.println("Verifying the Model S home Page");
+		assertTrue(driver.getTitle().contains("Model S"));
+		System.out.println(driver.getTitle() + "<------------------");
+	}
+
+	@When("^I click on Roadster link$")
+	public void i_click_on_Roadster_link() {
+		driver.findElement(By.linkText("ROADSTER")).click();
 
 	}
+
+	@Then("^Roadster home page should be displayed$")
+	public void roadster_home_page_should_be_displayed() {
+		System.out.println(driver.getTitle());
+		assertTrue(driver.getTitle().contains("Roadster"));
+	}
+
 }
--- first-cucumber-project/src/test/resources/tesla/homepage_test.feature
+++ first-cucumber-project/src/test/resources/tesla/homepage_test.feature
@@ -1,8 +1,13 @@
 Feature: Tesla home page
-# <- this is a comment leave your notes here.. using #
-Scenario: Model S link
-	Given I am on the home page
-	When I click on the model S link
-	Then Model S homepage should be displayed
-	
 
+  # <- this is a comment leave your notes here.. using #
+  Scenario: Model S link
+    Given I am on the home page
+    When I click on the model S link
+    Then Model S homepage should be displayed
+
+  Scenario: Roadster link
+    Given I am on the home page
+    When I click on Roadster link
+    Then Roadster home page should be displayed
+    # if any step in the scenario fails, 
